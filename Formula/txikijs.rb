1283ba3c0809b8f1dbc5e6f116fec47aadfc7ead
Jan T. Sott <git@idleberg.com>
2025-09-04 00:21:34 +0200

    refactor: install deps from brew

--- Formula/txikijs.rb
+++ Formula/txikijs.rb
@@ -1,16 +1,21 @@
 class Txikijs < Formula
   desc "Small and powerful JavaScript runtime"
   homepage "https://github.com/saghul/txiki.js"
-  url "https://github.com/saghul/txiki.js.git",
-      tag:      "v24.12.0",
-      revision: "793dd9d8bb1d54726ca4e7af1d951418e199175f"
+  url "https://codeload.github.com/saghul/txiki.js/tar.gz/refs/tags/v24.12.0"
+  sha256 "6ede6d543bffa60afa5fabf64d27a5465054e36505a1cb3ba8307a6fa3dc89c6"
   license "MIT"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
+  depends_on "libffi"
   depends_on "libtool" => :build
+  depends_on "libuv"
+  depends_on "mimalloc"
+  depends_on "quickjs"
+  depends_on "sqlite3"
   depends_on "texinfo" => :build
+  depends_on "wasm3"
 
   def install
     system "cmake", "-S", ".", "-B", "build", "-DCMAKE_POLICY_VERSION_MINIMUM=3.5", *std_cmake_args
