ab786fc57d251da13dfeb6e2de131750e49ccc91
Jan T. Sott <git@idleberg.com>
2025-09-04 22:30:30 +0200

    feat: add normcap

---
+++ Formula/normcap.rb
@@ -0,0 +1,22 @@
+class Normcap < Formula
+  desc "OCR powered screen-capture tool to capture information instead of images"
+  homepage "https://dynobo.github.io/normcap/"
+  url "https://github.com/dynobo/normcap/archive/refs/tags/v0.6.0.tar.gz"
+  version "0.6.0"
+  sha256 "aae35b423c2cc586ffc01cc556b4e7965133cc24cea296677efdac3de134cd50"
+  license "NOASSERTION"
+
+  depends_on "tesseract"
+  depends_on "tesseract-lang"
+  depends_on "uv" => :build
+  depends_on :macos
+
+  def install
+    system "uv", "run", "poe", "build"
+    prefix.install "build/normcap/mac/app/Normcap.app"
+  end
+
+  test dong tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
