4a9fde123d3026d328a10795a9880e7d3e5d867b
Jan T. Sott <git@idleberg.com>
2025-09-05 13:00:19 +0200

    fix: update os labels

--- Casks/dbxcli.rb
+++ Casks/dbxcli.rb
@@ -3,8 +3,8 @@ cask "dbxcli" do
   os macos: "darwin", linux: "linux"
 
   version "3.0.0"
-  sha256 macos: "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573",
-         linux: "806584e4758e59d753c1d7ed0fe97a7ae43fec0240c3bf4d388360bb51daf346"
+  sha256 arm:         "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573",
+         arm64_linux: "806584e4758e59d753c1d7ed0fe97a7ae43fec0240c3bf4d388360bb51daf346"
 
   url "https://github.com/dropbox/dbxcli/releases/download/v#{version}/dbxcli-#{os}-#{arch}"
   name "dbxcli"
