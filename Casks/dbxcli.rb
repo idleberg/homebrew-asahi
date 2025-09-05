8680a99250534ab34c4e90f32eaf4941605cbf0b
Jan T. Sott <git@idleberg.com>
2025-09-05 13:41:42 +0200

    refactor: handle architectures differently based on os

--- Casks/dbxcli.rb
+++ Casks/dbxcli.rb
@@ -4,8 +4,8 @@ cask "dbxcli" do
   arch arm: "arm", intel: "amd64"
 
   url "https://github.com/dropbox/dbxcli/releases/download/v#{version}/dbxcli-linux-#{arch}"
-  sha256 arm:   "806584e4758e59d753c1d7ed0fe97a7ae43fec0240c3bf4d388360bb51daf346",
-         intel: "3ff6e29675095063a5ba7101115cf7f809f3deddf14998ac61bf7a0330d736cb"
+  sha256 arm64_linux:  "806584e4758e59d753c1d7ed0fe97a7ae43fec0240c3bf4d388360bb51daf346",
+         x86_64_linux: "3ff6e29675095063a5ba7101115cf7f809f3deddf14998ac61bf7a0330d736cb"
   binary "dbxcli-linux-#{arch}", target: "dbxcli"
 
   on_macos do
