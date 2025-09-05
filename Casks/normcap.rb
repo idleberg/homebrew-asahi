93280723008cff5981dd3941ffe0fdd45b840da0
Jan T. Sott <git@idleberg.com>
2025-09-05 11:24:13 +0200

    chore: add postflight step

--- Casks/normcap.rb
+++ Casks/normcap.rb
@@ -1,5 +1,5 @@
 cask "normcap" do
-   arch arm: "arm64", intel: "x86_64"
+  arch arm: "arm64", intel: "x86_64"
 
   version "0.6.0"
   sha256 arm:   "69ce59d6ac3fb6420d62b7fc2ac5bf703b5b34eec05ad319a41b1c2f979f9ee2",
@@ -13,4 +13,13 @@ cask "normcap" do
   depends_on macos: ">= :ventura"
 
   app "NormCap.app"
+
+  postflight do
+    args = %W[
+      -d com.apple.quarantine
+      #{staged_path}/NormCap.app
+    ]
+
+    system_command "xattr", args: args
+  end
 end
