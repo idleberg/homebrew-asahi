66aed1a5da95d6f6efad0b16d0ece105694e1333
Jan T. Sott <git@idleberg.com>
2025-09-05 15:52:51 +0200

    refactor: handle architectures differently based on os

--- Casks/dbxcli.rb
+++ Casks/dbxcli.rb
@@ -1,10 +1,13 @@
 cask "dbxcli" do
+  arch arm: "arm", intel: "amd64"
+  os macos: "darwin", linux: "linux"
+
   version "3.0.0"
+  sha256 intel:        "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573",
+         arm64_linux:  "806584e4758e59d753c1d7ed0fe97a7ae43fec0240c3bf4d388360bb51daf346",
+         x86_64_linux: "3ff6e29675095063a5ba7101115cf7f809f3deddf14998ac61bf7a0330d736cb"
 
-  url "https://github.com/dropbox/dbxcli/releases/download/v#{version}/dbxcli-darwin-amd64"
-  sha256 "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
-  binary "dbxcli-darwin-amd64", target: "dbxcli"
-
+  url "https://github.com/dropbox/dbxcli/releases/download/v#{version}/dbxcli-#{os}-#{arch}"
   name "dbxcli"
   desc "Command-line client for Dropbox"
   homepage "https://github.com/dropbox/dbxcli"
@@ -14,16 +17,7 @@ cask "dbxcli" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :el_capitan"
-
-  postflight do
-    args = %W[
-      -d com.apple.quarantine
-      #{staged_path}/dbxcli
-    ]
-
-    system_command "xattr", args: args
-  end
+  binary "dbxcli-#{os}-#{arch}", target: "dbxcli"
 
   caveats do
     requires_rosetta
