598b1788d750a7200c5e1fb0cac61becb54927a4
Jan T. Sott <git@idleberg.com>
2025-09-05 12:42:04 +0200

    refactor: handle architectures differently based on os

--- Casks/dbxcli.rb
+++ Casks/dbxcli.rb
@@ -1,16 +1,23 @@
 cask "dbxcli" do
-  arch arm: "arm", intel: "amd64"
-  os macos: "darwin", linux: "linux"
-
   version "3.0.0"
-  sha256 intel:        "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573",
-         arm64_linux:  "806584e4758e59d753c1d7ed0fe97a7ae43fec0240c3bf4d388360bb51daf346",
-         x86_64_linux: "3ff6e29675095063a5ba7101115cf7f809f3deddf14998ac61bf7a0330d736cb"
 
-  url "https://github.com/dropbox/dbxcli/releases/download/v#{version}/dbxcli-#{os}-#{arch}"
+  on_macos do
+    # macOS only has AMD64 build available
+    url "https://github.com/dropbox/dbxcli/releases/download/v#{version}/dbxcli-darwin-amd64"
+    sha256 "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
+    binary "dbxcli-darwin-amd64", target: "dbxcli"
+  end
+
+  on_linux do
+    arch arm: "arm", intel: "amd64"
+
+    url "https://github.com/dropbox/dbxcli/releases/download/v#{version}/dbxcli-linux-#{arch}"
+    sha256 arm:   "806584e4758e59d753c1d7ed0fe97a7ae43fec0240c3bf4d388360bb51daf346",
+           intel: "3ff6e29675095063a5ba7101115cf7f809f3deddf14998ac61bf7a0330d736cb"
+    binary "dbxcli-linux-#{arch}", target: "dbxcli"
+  end
+
   name "dbxcli"
   desc "Command-line client for Dropbox"
   homepage "https://github.com/dropbox/dbxcli"
-
-  binary "dbxcli-#{os}-#{arch}", target: "dbxcli"
 end
