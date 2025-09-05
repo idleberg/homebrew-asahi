ddc3c17a50526e165baa92e9feb0d9a60b1598b1
Jan T. Sott <git@idleberg.com>
2025-09-05 12:25:26 +0200

    fix: add verified url

--- Casks/normcap.rb
+++ Casks/normcap.rb
@@ -5,7 +5,8 @@ cask "normcap" do
   sha256 arm:   "69ce59d6ac3fb6420d62b7fc2ac5bf703b5b34eec05ad319a41b1c2f979f9ee2",
          intel: "de620d4bd4b9db0f24fc354f9db29a87ae32c0ccc6ff63cbb34456d175ff0e5e"
 
-  url "https://github.com/dynobo/normcap/releases/download/v#{version}/NormCap-#{version}-#{arch}-macOS.dmg"
+  url "https://github.com/dynobo/normcap/releases/download/v#{version}/NormCap-#{version}-#{arch}-macOS.dmg",
+      verified: "github.com/dynobo/normcap"
   name "NormCap"
   desc "OCR-powered screenshot tool to capture text instead of images"
   homepage "https://dynobo.github.io/normcap/"
@@ -20,12 +21,14 @@ cask "normcap" do
   app "NormCap.app"
 
   postflight do
-    args = %W[
-      -d com.apple.quarantine
-      #{staged_path}/NormCap.app
-    ]
+    on_macos do
+      args = %W[
+        -d com.apple.quarantine
+        #{staged_path}/NormCap.app
+      ]
 
-    system_command "xattr", args: args
+      system_command "xattr", args: args
+    end
   end
 
   zap trash: [
