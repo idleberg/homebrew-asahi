d08c21881a182910141d8be6214c36e38123fb33
Jan T. Sott <git@idleberg.com>
2026-05-04 20:15:51 +0200

    chore: bump dent to 0.5.0

--- Formula/dent.rb
+++ Formula/dent.rb
@@ -1,8 +1,8 @@
 class Dent < Formula
   desc "Opinionated code formatter for NSIS scripts"
   homepage "https://github.com/idleberg/node-dent-cli"
-  url "https://github.com/idleberg/node-dent-cli/archive/refs/tags/v0.4.4.tar.gz"
-  sha256 "35db353e85a29c7aeb40dcc6b535643f78b9006ca467a45b6682223f2c74de02"
+  url "https://github.com/idleberg/node-dent-cli/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "ea5607f02c436c12a9079272ea8e5e54a6907cc7bcc4c0952f179f5fbf40bac4"
   license "MIT"
   head "https://github.com/idleberg/node-dent-cli.git", branch: "main"
 
