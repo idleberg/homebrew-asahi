bb96f0875c6255616494db7fcf9d3b8f8cb6e20e
Jan T. Sott <git@idleberg.com>
2026-05-13 00:44:58 +0200

    fix: add missing char in checksum

--- Formula/ardent.rb
+++ Formula/ardent.rb
@@ -2,7 +2,7 @@ class Ardent < Formula
   desc "Opinionated formatter for NSIS scripts"
   homepage "https://github.com/idleberg/ardent"
   url "https://github.com/idleberg/ardent/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "71e53eaa8c9ccf896bf267c7ab03bb16f3b4ac047c299811f82cfdf4b002995"
+  sha256 "71e53eaa8c9ccf896bf267c7ab03bb16f3b4ac047c299811f82cfdf4b002995d"
   license "MIT"
 
   depends_on "rust" => :build
