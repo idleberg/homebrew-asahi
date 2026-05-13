e8161429d7ea190d74be5b311e1805bd8a50b11f
Jan T. Sott <git@idleberg.com>
2026-05-13 23:26:02 +0200

    chore: define head

--- Formula/ardent.rb
+++ Formula/ardent.rb
@@ -4,6 +4,7 @@ class Ardent < Formula
   url "https://github.com/idleberg/ardent/archive/refs/tags/v0.3.0.tar.gz"
   sha256 "59a7b1cf95d86bac6766605f90773499bcbc11da06d6451453b1c2ac9b6d505e"
   license "MIT"
+  head "https://github.com/idleberg/ardent.git", branch: "main"
 
   depends_on "rust" => :build
 
