b7c713e874d1777e1e2b20fee1d8716a41c762b9
Jan T. Sott <git@idleberg.com>
2026-05-14 12:06:28 +0200

    chore: bump hashman

--- Formula/hashman.rb
+++ Formula/hashman.rb
@@ -1,8 +1,8 @@
 class Hashman < Formula
   desc "Command-line tool to calculate multiple checksums at once"
   homepage "https://github.com/idleberg/hashman"
-  url "https://github.com/idleberg/hashman/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7b525f1816895a013c4c6d1ebb324199fdf42f2d34fae61a38e707ece3f6c8ab"
+  url "https://github.com/idleberg/hashman/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "4c554a62b5ea073e28e3275279b79cd2b51ff4296b9081be0f884b2776f9ac37"
   license "MIT"
   head "https://github.com/idleberg/hashman.git", branch: "main"
 
