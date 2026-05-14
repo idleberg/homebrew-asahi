c65ec10c59738d916927696ec03492cf2741a4c5
Jan T. Sott <git@idleberg.com>
2026-05-15 01:28:44 +0200

    chore: update go-hashman

--- Formula/hashman.rb
+++ Formula/hashman.rb
@@ -1,8 +1,8 @@
 class Hashman < Formula
   desc "Command-line tool to calculate multiple checksums at once"
   homepage "https://github.com/idleberg/hashman"
-  url "https://github.com/idleberg/hashman/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "4c554a62b5ea073e28e3275279b79cd2b51ff4296b9081be0f884b2776f9ac37"
+  url "https://github.com/idleberg/hashman/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "e46caad27a86fee5c98b92d5ea63791b5252e7acaa86be9851cbd1424daddd57"
   license "MIT"
   head "https://github.com/idleberg/hashman.git", branch: "main"
 
