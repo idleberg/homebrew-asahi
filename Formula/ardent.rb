42bb64834863cb12b0d3ee40afadc8c190436a37
Jan T. Sott <git@idleberg.com>
2026-05-14 12:08:38 +0200

    chore: bump ardent

--- Formula/ardent.rb
+++ Formula/ardent.rb
@@ -1,8 +1,8 @@
 class Ardent < Formula
   desc "Opinionated formatter for NSIS scripts"
   homepage "https://github.com/idleberg/ardent"
-  url "https://github.com/idleberg/ardent/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "59a7b1cf95d86bac6766605f90773499bcbc11da06d6451453b1c2ac9b6d505e"
+  url "https://github.com/idleberg/ardent/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "170ba20d0c6b291840ecb6a9598dd15cc1ff44517b1b559b97d5e1fe3b05babb"
   license "MIT"
   head "https://github.com/idleberg/ardent.git", branch: "main"
 
