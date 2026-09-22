74b8968057bce1cd19a059ba2706478a1176e9d0
Jan T. Sott <git@idleberg.com>
2026-09-22 17:35:10 +0200

    chore: bump dent-desktop to v0.2.7

--- Casks/dent-desktop.rb
+++ Casks/dent-desktop.rb
@@ -1,6 +1,6 @@
 cask "dent-desktop" do
-  version "0.2.6"
-  sha256  "45b9819506ae2c8f3c410776f5aa68bb8fb06b14a2d21b224782ac0320d90426"
+  version "0.2.7"
+  sha256  "0bad78c8fa389708d90c804b856833ae49d20707e441a16acdc3e821b60f00a9"
 
   url "https://github.com/idleberg/dent-desktop/releases/download/v#{version}/dent-#{version}-darwin.zip"
   name "Dent"
